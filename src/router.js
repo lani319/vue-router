356fe7171d89d6ff00a482185bf7caf22f76c5d2
yoonhakeo <lani319@gmail.com>
2022-04-08 13:49:53 +0900

    start

---
+++ src/router.js
@@ -0,0 +1,26 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      //뷰티파이 라는것을 이용해서 작성 
+      path: '/', //주소
+      name: 'home',
+      component: Home //주소와 연결하는 컴포넌트 선언
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    }
+  ]
+})
